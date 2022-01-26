eed902c3a06c51688b7218095af141f3948753ed
SeanS27 <sean.stephenson@iinet.net.au>
2022-01-26 15:54:35 +0800

    Changed ModToolTier to use simpler ForgeTier

--- src/main/java/com/shadow_gry/emeraldtools/tools/ModToolTier.java
+++ src/main/java/com/shadow_gry/emeraldtools/tools/ModToolTier.java
@@ -1,61 +1,13 @@
 package com.shadow_gry.emeraldtools.tools;
 
-import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
+import net.minecraftforge.common.Tags;
 
-import java.util.function.Supplier;
-
-public enum ModToolTier implements Tier {
+public class ModToolTier {
 
     // TODO: Balance sample values.
-    EMERALD(800, 7.0F, 3.0F, 3, 12, () -> {
-        return Ingredient.of(Items.EMERALD);
-    });
-
-    private int maxUses;
-    private float efficiency;
-    private float attackDamage;
-    private int harvestLevel;
-    private int enchantability;
-    private Supplier<Ingredient> repairMaterial;
-
-    ModToolTier(int maxUses, float efficiency, float attackDamage, int harvestLevel, int enchantability, Supplier<Ingredient> repairMaterial) {
-        this.maxUses = maxUses;
-        this.efficiency = efficiency;
-        this.attackDamage = attackDamage;
-        this.harvestLevel = harvestLevel;
-        this.enchantability = enchantability;
-        this.repairMaterial = repairMaterial;
-    }
-
-    @Override
-    public int getUses() {
-        return maxUses;
-    }
-
-    @Override
-    public float getSpeed() {
-        return efficiency;
-    }
-
-    @Override
-    public float getAttackDamageBonus() {
-        return attackDamage;
-    }
-
-    @Override
-    public int getLevel() {
-        return harvestLevel;
-    }
-
-    @Override
-    public int getEnchantmentValue() {
-        return enchantability;
-    }
-
-    @Override
-    public Ingredient getRepairIngredient() {
-        return repairMaterial.get();
-    }
+    public static final ForgeTier EMERALD = new ForgeTier(3, 800, 7.0F, 3.0F, 12,
+            Tags.Blocks.NEEDS_GOLD_TOOL, () -> Ingredient.of(Items.EMERALD));
 }
